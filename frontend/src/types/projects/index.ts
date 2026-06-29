ba54179323b49a5126e9b0e252f5aa6e8be52a94
NguyenAnhDuc42 <144120505+NguyenAnhDuc42@users.noreply.github.com>
2026-06-29 18:56:26 +0700

    f

--- frontend/src/types/projects/index.ts
+++ frontend/src/types/projects/index.ts
@@ -1,7 +1,6 @@
 export * from './task-record';
 export * from './folder-record';
 export * from './space-record';
-export * from './workflow-record';
 export * from './comment-record';
 export * from './assignee-record';
 export * from './attachment-record';
